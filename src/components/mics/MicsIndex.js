b80f12344525acd0dabdfca86b65c820eb7661e9
EricCSheppard <EricCSheppard@gmail.com>
2023-07-21 12:11:43 -0400

    Post it rework begun

--- src/components/mics/MicsIndex.js
+++ src/components/mics/MicsIndex.js
@@ -43,24 +43,24 @@ const MicsIndex = (props) => {
     const micCards = mics.map(mic => (
         
         
-        <Card key={ mic.id } style={{ width: '30%', margin: 5 }}>
-            <Card.Header>{ mic.make} { mic.model }</Card.Header>
-            <Card.Body>
-                <Card.Text>
-                    <Link to={`/mics/${mic.id}`} className='btn btn-info'>View { mic.make} { mic.model }</Link>
-                </Card.Text>
-                { mic.owner ?
-                <Card.Footer>
-                    owner: {mic.owner.email}     
-                </Card.Footer>
-                : null }
-            </Card.Body>
-        </Card>
-        // <div class='post-it'>
-        //     <p>{ mic.model } </p><br/>
-        //     <p> { mic.make } </p>
-        //     <Link to={`/mics/${mic.id}`}>View { mic.make} { mic.model }</Link>
-        // </div>
+        // <Card key={ mic.id } style={{ width: '30%', margin: 5 }}>
+        //     <Card.Header>{ mic.make} { mic.model }</Card.Header>
+        //     <Card.Body>
+        //         <Card.Text>
+        //             <Link to={`/mics/${mic.id}`} className='btn btn-info'>View { mic.make} { mic.model }</Link>
+        //         </Card.Text>
+        //         { mic.owner ?
+        //         <Card.Footer>
+        //             owner: {mic.owner.email}     
+        //         </Card.Footer>
+        //         : null }
+        //     </Card.Body>
+        // </Card>
+        <div class='post-it'>
+            <p>{ mic.model } </p><br/>
+            <p> { mic.make } </p>
+            <Link to={`/mics/${mic.id}`}>View { mic.make} { mic.model }</Link>
+        </div>
     ))
     return (
         <div className="container-md" style= { cardContainerStyle }>
